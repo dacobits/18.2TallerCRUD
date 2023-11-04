483079fb8438d15ccef0b95ab4076a31be869e06
Dacobits <103399198+Dacobits@users.noreply.github.com>
2023-11-04 10:54:41 -0300

    Avance (Falta Pauta 5)

--- scripts/index.js
+++ scripts/index.js
@@ -1,132 +1,148 @@
 document.addEventListener("DOMContentLoaded", () => {
-
-    buscarID();
-    eliminarUser();
-    nuevoUser();
-
+  buscarID();
+  eliminarUser();
+  nuevoUser();
+  modificarUser();
 });
 
-const url = "https://65423647f0b8287df1ffb4a9.mockapi.io/users"
 
-let results = document.getElementById('results');
+const url = "https://65423647f0b8287df1ffb4a9.mockapi.io/users";
+let results = document.getElementById("results");
 
 function mostrarDatos() {
-    results.textContent = '';
-    fetch(url)
-        .then(resp => resp.json())
-        .then(data => {
-
-            data.forEach(element => {
-                let li = document.createElement('li');
-                let nombre = element.name;
-                let apellido = element.lastname;
-                let id = element.id;
-                li.innerHTML = `
+  results.textContent = "";
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => {
+      data.forEach((element) => {
+        let li = document.createElement("li");
+        let nombre = element.name;
+        let apellido = element.lastname;
+        let id = element.id;
+        li.innerHTML = `
             <li>ID: ${id}</li>
             <li>NAME: ${nombre}</li>
             <li>LASTNAME: ${apellido}</li>                    
         `;
-                results.appendChild(li);
-            });
-        });
-
+        results.appendChild(li);
+      });
+    });
 }
-
 
 function buscarID() {
+  let buscar = document.getElementById("btnGet1");
+  let input = document.getElementById("inputGet1Id");
+  let results = document.getElementById("results");
 
-    let buscar = document.getElementById("btnGet1");
-    let input = document.getElementById('inputGet1Id');
+  buscar.addEventListener("click", () => {
+    console.log("hiciste click");
+    results.innerText = "";
 
-    buscar.addEventListener('click', () => {
-        console.log('hiciste click')
-        results.innerText = ''
-
-        fetch(url)
-            .then(resp => resp.json())
-            .then(data => {
-                let resBusqueda = data.find((element) => element.id == input.value)
-
-                if (resBusqueda) {
-                    let li = document.createElement('li');
-                    let nombre = resBusqueda.name;
-                    let apellido = resBusqueda.lastname;
-                    let id = resBusqueda.id;
-                    li.innerHTML = `
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        if (input.value === "") {
+          mostrarDatos();
+        } else {
+          let resBusqueda = data.find((element) => element.id == input.value);
+          let li = document.createElement("li");
+          let nombre = resBusqueda.name;
+          let apellido = resBusqueda.lastname;
+          let id = resBusqueda.id;
+          li.innerHTML = `
                         <li>ID: ${id}</li>
                         <li>NAME: ${nombre}</li>
-                        <li>LASTNAME: ${apellido}</li>                    
-                        `;
-                    results.appendChild(li);
-                }
-            })
-    })
+                        <li>LASTNAME: ${apellido}</li>
+                    `;
+          results.appendChild(li);
+        }
+      });
+  });
 }
 
-
 function nuevoUser() {
-    // post 
+  let agregar = document.getElementById("btnPost");
 
-    let agregar = document.getElementById("btnPost");
+  agregar.addEventListener("click", () => {
+    let name = document.getElementById("inputPostNombre").value;
+    let lastname = document.getElementById("inputPostApellido").value;
 
-    agregar.addEventListener("click", () => {
-
-        let name = document.getElementById("inputPostNombre").value;
-        let lastname = document.getElementById("inputPostApellido").value;
-
-        fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                name: name,
-                lastname: lastname
-            })
-        })
-            .then(resp => resp.json())
-            .then(data => {
-                console.log(data)
-                mostrarDatos();
-            })
-
-    });
-
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        lastname: lastname,
+      }),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        mostrarDatos();
+      });
+  });
 }
 
 function modificarUser() {
-    // put
-    let modificar = document.getElementById("btnPut");
-    let modInput = document.getElementById("inputPutId").value;
-    modificar.addEventListener("click", () => {
+  let modificar = document.getElementById("btnPut");
+  let modInput = document.getElementById("inputPutId");
+  let guardar = document.getElementById("guardar");
+  let nombre = document.getElementById("name");
+  let apellido = document.getElementById("lastname");
 
-    });
+  modificar.addEventListener("click", () => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        resBusqueda = data.find((element) => element.id == modInput.value);
+
+        nombre.value = resBusqueda.name;
+        apellido.value = resBusqueda.lastname;
+
+        $("#modal").modal("show");
+      });
+  });
+
+  guardar.addEventListener("click", () => {
+    let datosActualizados = {
+      name: nombre.value,
+      lastname: apellido.value,
+    };
+
+    fetch(url + "/" + resBusqueda.id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(datosActualizados),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        mostrarDatos();
+      });
+
+    $("#modal").modal("hide");
+  });
 }
 
 function eliminarUser() {
-    // delete
-    let borrar = document.getElementById("btnDelete");
-    let delInput = document.getElementById("inputDelete")
-    console.log(delInput.value)
-    borrar.addEventListener("click", () => {
-        fetch(url + "/" + delInput.value, {
-            method: 'DELETE',
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8'
-            }
-
-        })
-            .then(resp => resp.json())
-            .then(data => {
-                console.log(data)
-                mostrarDatos();
-            })
-
-    });
-
+  let borrar = document.getElementById("btnDelete");
+  let delInput = document.getElementById("inputDelete");
+  
+  console.log(delInput.value);
+  borrar.addEventListener("click", () => {
+    fetch(url + "/" + delInput.value, {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        mostrarDatos();
+      });
+  });
 }
-
-
-
-
-
